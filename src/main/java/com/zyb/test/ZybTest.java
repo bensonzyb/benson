1aea84eddc80e8b1cf50d5bf7dbf64ceea8fb628
zyb <zhuo.youbei@yoyosys.com.cn>
2019-11-12 15:58:21 +0800

    Initial commit

---
+++ src/main/java/com/zyb/test/ZybTest.java
@@ -0,0 +1,13 @@
+package test;
+
+public class ZybTest {
+
+    public static void main(String[] args) {
+        String zyb="123456";
+        if(zyb.equals("123456")){
+            System.out.println("相等！！");
+        }
+
+    }
+
+}
